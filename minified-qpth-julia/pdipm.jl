b69c5eb8c5a27b80a00a52b22c09f0618b2073ed
Akshay Sharma <contact@imakshay.com>
2020-05-13 14:34:40 +0530

    add factorization code

--- minified-qpth-julia/pdipm.jl
+++ minified-qpth-julia/pdipm.jl
@@ -1,33 +1,106 @@
 using LinearAlgebra
 
-function pre_factor_kkt(Q::Array{Float64},G::Array{Float64},A::Array{Float64},nineq::Inf64, nz::Inf64, neq::Inf64)
+"""
+get LU decomposition with partial pivoting
+"""
+function lu_pp(X)
+    X_copy = copy(X)
+    data, pivots = LAPACK.getrf!(X_copy) # mutates the matrix
+    return data, pivots
+end
+
+
+function pre_factor_kkt(Q::Array{Float64},G::Array{Float64},A::Array{Float64},nineq::Int64, neq::Int64)
     
     # S = [ A Q^{-1} A^T    A Q^{-1} G^T         ]
     #     [ G Q^{-1} A^T    G Q^{-1} G^T + D^{-1}]
 
     # Q must be SPD else code will throw a Singular Exception
     # Q is always a square matrix
-    
-    ##############
-    # TODO implementation incomplete
 
     Q_LU = lu(Q)
     R = G * (Q_LU \ G')
+    S_LU_pivots = Array(1:neq+nineq)
     
     if neq > 0
-        invQ_AT = Q \ A'
+        invQ_AT = Q_LU \ A'
         A_invQ_AT = A * invQ_AT
         G_invQ_AT = G * invQ_AT
+
+        LU_A_invQ_AT = lu_pp(A_invQ_AT)
         
         T = A_invQ_AT \ G_invQ_AT'
         
-        S = [ A_invQ_AT  A*(Q\G');
-              G_invQ_AT  zeros(nineq, nineq)]
+        S_LU_data = [ LU_A_invQ_AT[1]  A*(Q\G');
+                      G_invQ_AT  zeros(nineq, nineq)]
+
+        S_LU_pivots[1:neq] = LU_A_invQ_AT[2]
         
-        R = R - G_invQ_AT*T
+        R -= G_invQ_AT*T
     else
-        S = zeros(nineq,nineq)
+        S_LU_data = zeros(nineq,nineq)
     end
     
-    return Q_LU, S, R
+    S_LU = [S_LU_data, S_LU_pivots]
+    return Q_LU, S_LU, R
 end 
+
+
+""" Factor the U22 block that we can only do after we know D. """
+function factor_kkt(S_LU::Array{Float64},R::Array{Float64},d::Array{Float64},nineq::Int64, neq::Int64)
+    factor_kkt_eye = Matrix{Bool}(I, nineq, nineq)
+    T = copy(R)
+    T[factor_kkt_eye] += (1. ./ d)
+
+    T_LU = lu_pp(T)
+
+    # TODO: how to re-pivot?
+    # oldPivotsPacked = S_LU[2][neq+1:neq+nineq] - neq
+    # oldPivots, _, _ = unpack(T_LU[1], oldPivotsPacked)
+    # newPivotsPacked = T_LU[1]
+    # newPivots, _, _ = lu_unpack(T_LU[1], newPivotsPacked)
+
+    # # Re-pivot the S_LU_21 block.
+    # if neq > 0
+    #     S_LU_21 = S_LU[1][neq+1:neq+nineq, 1:neq]
+    #     S_LU[1][neq+1:neq+nineq,1:neq] = newPivots' *(oldPivots*S_LU_21)
+    # end
+
+    # # Add the new S_LU_22 block pivots.
+    # S_LU[2][neq+1:neq+nineq] = newPivotsPacked + neq
+
+    # # Add the new S_LU_22 block.
+    # S_LU[1][neq+1:neq+nineq, neq+1:neq+nineq] = T_LU[1]
+
+    return S_LU
+end
+
+
+""" Solve KKT equations for the affine step"""
+function solve_kkt(Q_LU::Array{Float64}, d::Array{Float64}, G::Array{Float64}, A::Array{Float64}, S_LU::Array{Float64}, rx::Array{Float64}, rs::Array{Float64}, rz::Array{Float64}, ry::Array{Float64}, neq::Int64)
+    invQ_rx = Q_LU \ rx
+    if neq > 0
+        h = hcat(invQ_rx * A' - ry, invQ_rx * G' + rs./d - rz)
+    else
+        h = invQ_rx * G' + rs./d - rz
+    end
+
+    w = -(S_LU \ h)
+    len = size(w)[1]
+
+    g1 = -rx - w[neq+1:len] * G
+    if neq > 0
+        g1 -= w[1:neq] * A
+    end
+    g2 = -rs - w[neq+1:len]
+
+    dx = Q_LU \ g1
+    ds = g2 ./ d
+    dz = w[neq+1:len]
+    if neq > 0
+        dy = w[1:neq]
+    else
+        dy = nothing
+    end
+    return dx, ds, dz, dy
+end
--- minified-qpth-julia/qp.jl
+++ minified-qpth-julia/qp.jl
@@ -1,5 +1,6 @@
 using LinearAlgebra;
 include("solver.jl");
+include("pdipm.jl");
 
 struct QP
     Q::Array{Float64}
@@ -23,18 +24,19 @@ struct QP
     function QP(Q_::Array{Float64}, p_::Array{Float64}, G_::Array{Float64}, h_::Array{Float64}, A_::Array{Float64}, b_::Array{Float64})
         nineq_, nz_ = size(G_)
         neq_ = size(A_)[1]
+    
+        # check if Q is SPD
+        if !isposdef(Q_)
+            throw(ArgumentError("Matrix Q should be positive semidefinite"))
+        end
+
+        @assert (neq_ > 0 || nineq_ > 0)
+        
         new(Q_, p_, G_, h_, A_, b_, neq_, nineq_, nz_,zeros(1),zeros(nz_),zeros(nineq_),zeros(neq_),zeros(nineq_))
     end
 end
 
-function forward(qp::QP)
-    # check if Q is SPD
-    if !isposdef(qp.Q)
-        throw(ArgumentError("Matrix Q should be positive semidefinite"))
-    end
-
-    @assert (qp.neq > 0 || qp.nineq > 0)
-    
+function forward(qp::QP)  
     val, zhat, nu, lam, slacks = forward_single(qp.Q, qp.p, qp.G, qp.h, qp.A, qp.b)
 
     qp.val  .+= val
@@ -48,7 +50,29 @@ end
 
 
 function backward(qp::QP, dl_dzhat::Array{Float64})
-    # TODO implementation incomplete
-    # Q_LU, S_LU, R = pdipm_b.pre_factor_kkt(qp.Q, qp.G, qp.A, qp.nineq,qp.nz,qp.neq)
-    return 1
+    zhat, Q, p, G, h, A, b = qp.zhat, qp.Q, qp.p, qp.G, qp.h, qp.A, qp.b
+    neq, nineq = qp.neq, qp.nineq
+    lam, slacks, nu = qp.lam, qp.slacks, qp.nu
+
+    Q_LU, S_LU, R = pre_factor_kkt(Q, G, A, nineq, neq)
+
+    d = clamp.(lam,1e-8,1e8) ./ clamp.(slacks,1e-8,1e8)
+
+    S_LU = factor_kkt(S_LU, R, d, nineq, neq)
+    dx, _, dlam, dnu = solve_kkt(Q_LU, d, G, A, S_LU, dl_dzhat,
+                                 zeros(nineq),zeros(nineq), zeros(neq))
+
+    dp = dx
+    dG = (dlam*zhat) + (lam*dx)
+    dh = -dlam
+    if neq > 0
+        dA = (dnu*zhat) + (nu*dx)
+        db = -dnu
+    else
+        dA, db = nothing, nothing
+    end
+    dQ = 0.5 * ((dx*zhat) + (zhat*dx))
+
+    grads = (dQ, dp, dG, dh, dA, db)
+    return grads
 end
